780615e78b0ee10c4a8488bfdced7fb67927af68
zhangyf511 <498104227@qq.com>
2022-06-17 00:08:23 +0800

    单选切换同一坐标下天地图和高德地图

--- index.js
+++ index.js
@@ -14,8 +14,11 @@ const layer = new TileLayer(
     subdomains: '1234',
   }
 );
-const tdtLayer = new TileLayer(
-  'http://t0.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={x}&TILECOL={y}&tk=c2190c0317e3b3e264124b7fc7a9ec7d'
-);
+// const tdtoneLayer = new TileLayer(
+//   'http://t0.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={x}&TILECOL={y}&tk=c2190c0317e3b3e264124b7fc7a9ec7d'
+// );
+// const tdttwoLayer = new TileLayer(
+//   'http://http://t0.tianditu.gov.cn/vec_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={x}&TILECOL={y}&tk=c2190c0317e3b3e264124b7fc7a9ec7d'
+// );
 layer.addTo(map);
 map.setView([39, 117], 10);
